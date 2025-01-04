86599bd2d19885e22305f04223b5025ec1e6061a
SameDev <conradtsamuel@gmail.com>
2025-01-04 16:00:13 -0300

    coisa funcionando

--- pages/login.php
+++ pages/login.php
@@ -5,14 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de Login de Usuários</title>
-    <link rel="stylesheet" href="userlog.css">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>/public/styles/userlog.css">
 </head>
 
 <body>
     <div class="container">
         <h1>Login de Usuário</h1>
         <form action="#" method="POST">
-        <?php echo INCLUDE_PATH; ?>
             <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email" placeholder="Digite seu email" required>
@@ -23,7 +22,7 @@
             </div>
             <button type="submit" class="btn">Login</button>
             <div class="footer">
-                Ainda não tem uma conta? <a href="cadastro">Cire sua conta.</a>
+                Ainda não tem uma conta? <a href="cadastro">Crie sua conta.</a>
             </div>
             <?php
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
